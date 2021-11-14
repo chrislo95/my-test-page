b58b46339b922c2bcc53042f34edc8e79d653720
test user <christianlopez641c@gmail.com>
2021-11-14 16:33:14 -0500

    changed a character in the title

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Wow I can't believe this worked!! 
+          Wow I can't believe this worked
         </h1>
 
         <p className={styles.description}>
